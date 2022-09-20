9cb90772c52e9415198be90627f33acb2a11e12b
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-09-20 22:57:43 +0300

    день 32
    ще трохи пописав камеру
    **колесико миші** маштабує
    **wasd** пересуває по декілька пікселів за натиск
    але робить усе це покищо не достатньо гарно
    доведеться ще попрацювати

--- code/game/Camera.lua
+++ code/game/Camera.lua
@@ -1,61 +1,70 @@
 
 ---@class objCamera
 local cam = {
-    timer_pos = 0,
-    timer_scale = 0,
-
+    -- позиція камери
     cam_pos_x = 0,
     cam_pos_y = 0,
-
-    cam_scale_w = 1,
-    cam_scale_h = 1,
-
-    cam_width = 0,
-    cam_height = 0,
-
+    
+    pushtimer_pos = 0,
     push_pos_x = 0,
     push_pos_y = 0,
 
-    push_scale_w = 1,
-    push_scale_h = 1,
+    -- маштабування камери
+    cam_scale = 1,
+ 
+    pushtimer_scale = 0,
+    push_scale = 1,
+    
+    -- розмір вікна
+    cam_width = 0,
+    cam_height = 0,
 }
+
+
+function cam:draw(...)
+    love.graphics.push()
+    love.graphics.translate(self.cam_pos_x, self.cam_pos_y)
+    love.graphics.scale(self.cam_scale, self.cam_scale)
+    for _, obj in ipairs({...}) do
+        obj:draw()
+    end
+    love.graphics.pop()
+    love.graphics.circle("line", self.cam_width / 2, self.cam_height / 2, 16, 32)
+end
 
 
 function cam:update(dt)
     self:updPos(dt)
     self:updScale(dt)
 end
-
 function cam:updScale(dt)
-    if self.timer_scale > 0 then
-        if dt > self.timer_scale then
-            self.cam_scale_w = self.push_scale_w
-            self.cam_scale_h = self.push_scale_h
-            self.timer_scale = 0
+    if self.pushtimer_scale > 0 then
+        if dt > self.pushtimer_scale then
+            self:setScale(self.push_scale)
+            self.pushtimer_scale = 0
         else
-            print(self.push_scale_w, self.push_scale_h)
-            local k = dt / self.timer_scale
-            self.cam_scale_w = (self.push_scale_w - self.cam_scale_w) * k + self.cam_scale_w
-            self.cam_scale_h = (self.push_scale_h - self.cam_scale_h) * k + self.cam_scale_h
-            self.timer_scale = self.timer_scale - dt
+            local k = dt / self.pushtimer_scale
+            self:setScale((self.push_scale - self.cam_scale) * k + self.cam_scale)
+            self.pushtimer_scale = self.pushtimer_scale - dt
+        end
+    end
+end
+function cam:updPos(dt)
+    if self.pushtimer_pos > 0 then
+        if dt > self.pushtimer_pos then
+            self:setPosition(self.push_pos_x, self.push_pos_y)
+            self.pushtimer_pos = 0
+        else
+            local k = dt / self.pushtimer_pos
+            self:setPosition(
+                (self.push_pos_x - self.cam_pos_x) * k + self.cam_pos_x,
+                (self.push_pos_y - self.cam_pos_y) * k + self.cam_pos_y
+            )
+            self.pushtimer_pos = self.pushtimer_pos - dt
         end
     end
 end
 
-function cam:updPos(dt)
-    if self.timer_pos > 0 then
-        if dt > self.timer_pos then
-            self.cam_pos_x = self.push_pos_x
-            self.cam_pos_y = self.push_pos_y
-            self.timer_pos = 0
-        else
-            local k = dt / self.timer_pos
-            self.cam_pos_x = (self.push_pos_x - self.cam_pos_x) * k + self.cam_pos_x
-            self.cam_pos_y = (self.push_pos_y - self.cam_pos_y) * k + self.cam_pos_y
-            self.timer_pos = self.timer_pos - dt
-        end
-    end
-end
 
 function cam:resetFrame()
     local w , h = love.graphics.getDimensions()
@@ -65,52 +74,62 @@ function cam:resetFrame()
     self.cam_width , self.cam_height = w , h
 end
 
-function cam:pushScaleAdd(w, h)
-    self:pushScale(self.push_scale_w + w, self.push_scale_h + (h or w))
+
+function cam:initScale(scale)
+    self:setScale(scale)
+    self:pushScale(scale, 0)
+end
+function cam:setScale(scale)
+    scale = math.max(Camera.scale_min, math.min(Camera.scale_max, scale))
+    self.cam_scale = scale
+end
+function cam:pushScale(scale, timer)
+    timer = timer or Camera.pushtimer_scale
+    self.pushtimer_scale = self.pushtimer_scale + timer
+    scale = math.min(4, math.max(1, scale))
+    local k1 = self.push_scale / scale
+    local k =  scale / self.push_scale
+    print(k, k1)
+    self.push_scale = scale
+    self:pushPosition(
+        self.push_pos_x * k1,
+        self.push_pos_y * k1,
+        timer
+    )
+end
+function cam:addScale(scale)
+    self:pushScale(self.push_scale + scale)
 end
 
-function cam:pushScale(w, h)
-    w = math.max(1, w)
-    h = math.max(1, h or w)
-    self.timer_scale = Camera.timer_scale
-    self.push_scale_w = w
-    self.push_scale_h = h
-end
 
-function cam:pushPosition(x, y)
-    self.timer_pos = Camera.timer_pos
-    self.push_pos_x = x
-    self.push_pos_y = y
+function cam:initPosition(x, y)
+    self:setPosition(x, y)
+    self:pushPosition(x, y, 0)
 end
-
-function cam:setScale(w, h)
-    if w > 0 or h > 0 then
-        self.cam_scale_w = w
-        self.cam_scale_h = h or w
-    end
-end
-
 function cam:setPosition(x, y)
     self.cam_pos_x = x
     self.cam_pos_y = y
 end
-
-function cam:draw(...)
-    love.graphics.push()
-    love.graphics.translate(self.cam_pos_x, self.cam_pos_y)
-    love.graphics.scale(self.cam_scale_w, self.cam_scale_h)
-    for _, obj in ipairs({...}) do
-        obj:draw()
-    end
-    love.graphics.pop()
+function cam:pushPosition(x, y, timer)
+    timer = timer or Camera.pushtimer_pos
+    self.pushtimer_pos = self.pushtimer_pos + timer
+    self.push_pos_x = x
+    self.push_pos_y = y
+end
+function cam:addPosition(x, y)
+    self:pushPosition(self.cam_pos_x + x, self.cam_pos_y + y)
 end
 
 
 ---@class gameCamera
 Camera = {
     obj = cam,
-    timer_pos = 0,5, -- за який час проходить пересування камери
-    timer_scale = 0.15, -- за який час проходить маштабування
+
+    pushtimer_pos = 0.25, -- за який час проходить пересування камери
+    pushtimer_scale = 0.15, -- за який час проходить маштабування
+
+    scale_min = 1,
+    scale_max = 4,
 }
 
 function Camera:new()
--- main.lua
+++ main.lua
@@ -30,13 +30,32 @@ function love.load(...)
     Map:load(1)
     
     c1 = Camera:new()
-    c1:setScale(2)
+    c1:initScale(2)
 end
 
 
 function love.keypressed(key)
     if key == 'escape' then love.event.push('quit') end
-    if key == 'f4' then gameScreenToggle() end
+    if key == 'f4' then
+        gameScreenToggle()
+        c1:resetFrame()
+    end
+
+    local step = 64
+
+    if key == 'w' then
+        c1:addPosition(0, step)
+    end
+    if key == 's' then
+        c1:addPosition(0, -step)
+    end
+
+    if key == 'a' then
+        c1:addPosition(step, 0)
+    end
+    if key == 'd' then
+        c1:addPosition(-step, 0)
+    end
 end
 
 
@@ -64,7 +83,7 @@ function love.mousepressed(x, y, button, istouch, presses) end
 
 
 function love.wheelmoved( x , y )
-    c1:pushScaleAdd(y * 0.5)
+    c1:addScale(y * 0.5)
 end
 
 
