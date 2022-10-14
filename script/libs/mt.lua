763e9e5bf3f2fbbcf9f4b8c7f415c9274a11aeed
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-10-14 13:31:46 +0300

    день 48
    (14.10.2022)
    
    ...отже, на чому я зупинився? переписати усе =.=
    
    але зпершу напишу маленьку бібліотеку mt.lua
    щоб трохи автоматизувати роботу з метатаблицями

---
+++ script/libs/mt.lua
@@ -0,0 +1,30 @@
+--[[
+
+objClass = {}
+
+~mtClass = {__index = objClass} <br />
+local obj = setmetatable({}, mtClass)~
+
+local obj = mt.getMt(objClass)
+
+]]
+local mt = {}
+
+local mts = {}
+
+function mt.setMt(obj, from)
+  print( type(obj), type(from) )
+  assert(type(from) == "table" and type(obj) == "table")
+  local key = tostring(from)
+  if not mts[key]
+  then mts[key] = {__index = from} end
+  return setmetatable(obj, mts[key])
+end
+
+function mt.setCall(objClass, fn)
+  assert(type(objClass) == "table")
+  local key = tostring(objClass)
+  mts[key].__call = fn
+end
+
+return mt
