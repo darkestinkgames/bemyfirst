a3f673ce3addedcd5c6fe581208079daba765432
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2024-02-20 12:35:14 +0200

    Create blank_gs.lua

---
+++ scripts/mods/blank_gs.lua
@@ -0,0 +1,21 @@
+
+local blank_gs = GState.new()
+-- function GState.blank_gs() GState.switch(blank_gs) end
+print('[blank_gs]')
+
+
+local _______________shorts_______________ = 0
+
+
+local la = love.audio
+local lf = love.filesystem
+local lg = love.graphics
+local ls = love.system
+local lw = love.window
+
+local isKey = love.keyboard.isDown
+local getMouse = love.mouse.getPosition
+
+
+local _______________mess_______________ = 0
+local _______________blank_gs_______________ = 0
